43836d2c3a72f4f38c77b72a54a44c0a80698fb3
Raul Ferrandez <ferrandezraul@gmail.com>
2013-12-30 12:33:58 +0100

    Started refinery with new scss style and with some data entered

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRefineryBlog::Application.config.secret_token = 'eebc78688c68466ff9334884807cd37ca01d4e599b46c5d7d8b1c91307410b4c9879fb1b2bb17ce2917fae267ff1161ea0540d4e83b3b3b97b28a783eb8e35c9'
