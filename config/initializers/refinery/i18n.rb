05c581786052a5ad2f56d6d42357d8f9b30e3811
Raul Ferrandez <raul.ferrandez@here.com>
2014-06-27 14:23:24 +0200

    Added catalan and german locale. Note catalan has wrong code in refinery 2.1.0. Next commit to upgrade to 2.1.1

--- config/initializers/refinery/i18n.rb
+++ config/initializers/refinery/i18n.rb
@@ -7,8 +7,8 @@ Refinery::I18n.configure do |config|
 
   # config.default_frontend_locale = :en
 
-  config.frontend_locales = [:en, :es]
+  config.frontend_locales = [:en, :es, :de, :ca]
 
-  config.locales = {:en=>"English", :es=>"Español" }
+  config.locales = {:en=>"English", :es=>"Español", :de=>"Deutsch", :ca=>"Catala" }
   # config.locales = {:en=>"English", :fr=>"Français", :nl=>"Nederlands", :pt=>"Português", :"pt-BR"=>"Português brasileiro", :da=>"Dansk", :nb=>"Norsk Bokmål", :sl=>"Slovenian", :es=>"Español", :it=>"Italiano", :de=>"Deutsch", :lv=>"Latviski", :ru=>"Русский", :sv=>"Svenska", :pl=>"Polski", :"zh-CN"=>"简体中文", :"zh-TW"=>"繁體中文", :el=>"Ελληνικά", :rs=>"Srpski", :cs=>"Česky", :sk=>"Slovenský", :ja=>"日本語", :bg=>"Български", :hu=>"Hungarian", :uk=>"Українська"}
 end
